7575b0796bcb9d1a28a3d87559e319a92c097293
chritter1310 <christianritter1310@gmail.com>
2026-09-01 23:46:55 +0200

    added live results for TTOP Series competitions

--- config.js
+++ config.js
@@ -1,3 +1,9 @@
 // config.js
+
+// SUPABASE PROJECT A (Website - Nennungen)
 export const SUPABASE_URL = "https://amjvuowsufiecsaentdn.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFtanZ1b3dzdWZpZWNzYWVudGRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAwNjQyNTUsImV4cCI6MjA5NTY0MDI1NX0.3ZKvO9srohn18zM1jRaPsZt5u2Hcq84t8JyU5EJ8iGE";
+
+// SUPABASE PROJECT B (Turnier-Software)
+export const SUPABASE_URL_B = "https://gtmsbtkvoqopugybelcj.supabase.co";
+export const SUPABASE_ANON_KEY_B = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd0bXNidGt2b3FvcHVneWJlbGNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MzU5MDUsImV4cCI6MjEwMzUxMTkwNX0.6uOw0p1M7WLNdcspvxJm72ZQXxOGiZblvQcU_xCyHiA";
